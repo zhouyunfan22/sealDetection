0e8094d6d2d892974f94c7d774ebaf016262c8af
何明会hmh <18911323634@163.com>
2020-12-02 21:34:43 +0800

    更名

--- Util/sealDetectionUtil.py
+++ Util/sealDetectionUtil.py
@@ -1,7 +1,7 @@
-from Util import pdf2Img
+from Util import pdf2ImgUtil
 from Util import sealInfo
 from Util import houghCircleUtil
-from Util import sealNumIsCorrect
+from Util import sealNumIsCorrectUtil
 def sealDetection(pdfName,pdfSrc):
     #获取该文件的印章参数
     sealInfoList=sealInfo.SealInfo(pdfName)
@@ -9,12 +9,12 @@ def sealDetection(pdfName,pdfSrc):
     minRadius=sealInfoList[1]
     maxRadius=sealInfoList[2]
     #将该文件转化为JPG
-    imglist = pdf2Img.pdf2Img(pdfSrc)
+    imglist = pdf2ImgUtil.pdf2Img(pdfSrc)
     #将Jpg输入基尔霍夫
     numlist=houghCircleUtil.detectCircles(imglist, minDist, minRadius,maxRadius)
     print(numlist)
     result = []
-    if sealNumIsCorrect.sealNumIsCorrect(pdfName, numlist, len(imglist))==False:
+    if sealNumIsCorrectUtil.sealNumIsCorrect(pdfName, numlist, len(imglist))==False:
         checkResult={
             "result": pdfName + "缺少印章",
             "context": ""
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from Util import sealDetection
+from Util import sealDetectionUtil
 
 pdfSrc = "D:/fingerprintImg/test/113.pdf"
 pdfName="调取证据清单"
 if __name__ == '__main__':
-   print(sealDetection.sealDetection(pdfName, pdfSrc))
+   print(sealDetectionUtil.sealDetection(pdfName, pdfSrc))
 
